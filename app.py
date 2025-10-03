972efc89e6af4a63612b69649a5ee4119de04b0d
James Clarke <jjjclarke@yahoo.com>
2025-10-03 14:36:27 +0100

    Refine Task class and add JSON function
    Signed-off-by: James Clarke <jjjclarke@yahoo.com>

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template
 
+class Task:
+    def __init__(self, task_id, name, description, status=0):
+        self.id = task_id # a unique identifier
+        self.name = name
+        self.description = description
+        self.status = status # 0 = to-do, 1 = doing, 2 = done
+
+    # Could make life easier in the future by returning tasks
+    # in a JSON format, might be good for data storage later
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "status": self.status
+        }
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-class Task:
-    def __init__(self, name, description, status):
-        self.name = name
-        self.description = description
-        self.status = status
-
-task1 = Task("Do work", "Doing my work", 1)
 if __name__ == ('__main__'):
     app.run()
