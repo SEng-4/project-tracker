e103e03e3fdd06d60c9321bc792e3a7128609d58
James Clarke <jjjclarke@yahoo.com>
2025-10-06 09:25:40 +0100

    Revert "Merge pull request #2 from SEng-4/AddedStatus"
    This reverts commit 4e8161e1f8daa160cc168e61d1b0fe77fafc734d, reversing
    changes made to 15a365a2acb09e4223dc8eac4d7f6ffeef160f94.

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template, jsonify
 
 class Task:
     def __init__(self, task_id, name, description, status=0):
-        self.id = task_id  # a unique identifier
+        self.id = task_id # a unique identifier
         self.name = name
         self.description = description
-        self.status = status  # 0 = to-do, 1 = doing, 2 = done
+        self.status = status # 0 = to-do, 1 = doing, 2 = done
 
+    # Could make life easier in the future by returning tasks
+    # in a JSON format, might be good for data storage later
     def to_dict(self):
         return {
             "id": self.id,
@@ -17,42 +19,23 @@ class Task:
 
 app = Flask(__name__)
 
-todo_tasks = []
-doing_tasks = []
-done_tasks = []
+tasks = []
 next_id = 0
 
 def create_task(name, description, status):
     global next_id
     task = Task(next_id, name, description, status)
-    if status == 0:
-        todo_tasks.append(task)
-    elif status == 1:
-        doing_tasks.append(task)
-    elif status == 2:
-        done_tasks.append(task)
+    tasks.append(task)
     next_id += 1
 
 @app.route('/')
 def index():
-    create_task("Test To-Do", "This is a to-do task.", 0)
-    create_task("Test Doing", "This is a doing task.", 1)
-    create_task("Test Done", "This is a done task.", 2)
-    return render_template(
-        'index.html',
-        todo_tasks=todo_tasks,
-        doing_tasks=doing_tasks,
-        done_tasks=done_tasks
-    )
+    create_task("Test Task", "Testing if this works...", 1)
+    return render_template('index.html', tasks=tasks)
 
 @app.route('/tasks', methods=["GET"])
 def list_tasks():
-    # Return all tasks grouped by status
-    return jsonify({
-        "todo": [task.to_dict() for task in todo_tasks],
-        "doing": [task.to_dict() for task in doing_tasks],
-        "done": [task.to_dict() for task in done_tasks]
-    })
+    return jsonify([task.to_dict() for task in tasks])
 
-if __name__ == '__main__':
+if __name__ == ('__main__'):
     app.run()
